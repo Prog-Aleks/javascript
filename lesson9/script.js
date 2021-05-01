bc693c4bc9d499c8e0466a72576f57ee49e7c98b
prog-aleks <prog.aleksey@gmail.com>
2021-05-01 23:20:39 +0300

    lesson9

---
+++ lesson9/script.js
@@ -0,0 +1,13 @@
+"use scrict";
+
+//const result = window.confirm("Are you here?");
+
+//const answer = + prompt("Вам есть 18","18");
+//console.log(typeof(answer + 5));
+
+const answer = [];
+answer[0] = prompt('как ваше имя','');
+answer[1] = prompt('как вашаи фамилия','');
+answer[2] = prompt('сколько лет','');
+
+document.write(answer);
